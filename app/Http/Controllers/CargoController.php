d063c8c6af74b0206e153827ac5da7da74bac2e1
Limber Rojas Villarroel <liroevi@hotmail.com>
2018-11-21 16:02:22 -0400

    Utilizacion de Vue.js en la elaboracion de CRUD con base de datos

---
+++ app/Http/Controllers/CargoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cargo;
+
+class CargoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $cargos = Cargo::orderBy('id', 'DESC')->paginate(2);
+
+        return [
+            'pagination' => [
+                'total'         => $cargos->total(),
+                'current_page'  => $cargos->currentPage(),
+                'per_page'      => $cargos->perPage(),
+                'last_page'     => $cargos->lastPage(),
+                'from'          => $cargos->firstItem(),
+                'to'            => $cargos->lastItem(),
+            ],
+            'cargos' => $cargos
+        ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'descripcion' => 'required'
+        ]);
+
+        Cargo::create($request->all());
+
+        return;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'descripcion' => 'required',
+        ]);
+
+        cargo::find($id)->update($request->all());
+
+        return;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cargos = cargo::findOrFail($id);
+        $cargos->delete();
+    }
+}
